35fa9ef4f2502ba925b54e1cca9d3ed8a8d65fcd
Donald Barrett <donald.barrett@learningworks.co.nz>
2019-10-30 09:21:19 +1300

    Updated to versin 2019102503 release 3.3.0.1

--- version.php
+++ version.php
@@ -10,7 +10,7 @@
  * @package openid
  */
 
-$plugin->version  = 2019102502;  // The current module version (Date: YYYYMMDDXX)
+$plugin->version  = 2019102503;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2015101600;  // Requires this Moodle version
 $plugin->release = '3.3.0.1';    // ELIS Component Version
 $plugin->component = 'auth_openid';
